c28af4b7f8e0bb3f54caf5e77b14dd843304ce7e
buketcuvelenk <73163419+buketcuvelenk@users.noreply.github.com>
2020-12-28 21:33:11 +0300

    Feiature UI branch initialization

--- src/mainfunctions/Main.java
+++ src/mainfunctions/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent parent = new BorderPane();
-		
+		System.out.println("New Branch");
 	}
 	
 	
