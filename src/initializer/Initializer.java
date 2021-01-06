f9944973b2d1a51f3798d23447a68aee68531d93
Hakan Ayaz <hakanayaz14159@outlook.com>
2021-01-06 23:55:24 +0300

    Initializer feature has started to be developed.

---
+++ src/initializer/Initializer.java
@@ -0,0 +1,22 @@
+package initializer;
+
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import model.Course;
+
+public class Initializer {
+	
+	private static LinkedHashMap<String ,Course> courseData;
+	
+	public void initialize() {
+		
+	}
+	
+	public void resolve() {
+		
+	}
+	
+	
+}
