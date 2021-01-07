0b86cbdb599b608b718402bcf76669e44bd12427
zeynepbetul <z.beetulkaya@gmail.com>
2021-01-07 17:45:03 +0300

    Added controllers for each menu item

--- src/MainFrame/MainViewController.java
+++ src/MainFrame/MainViewController.java
@@ -1,21 +1,44 @@
 package MainFrame;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-
-import java.io.IOException;
-
-import MainFrame.Main;
-
 public class MainViewController {
-	
+    @FXML
+    public void createSyllabus(ActionEvent actionEvent) {
+        System.out.println("Clicked On Item:");
+    }
+    @FXML
+    public void getSyllabus(ActionEvent actionEvent) {
+        System.out.println("Clicked On Item:");
+    }
+    @FXML
+    public void open(ActionEvent actionEvent) {
+        System.out.println("Clicked On Item:");
+    }
+    @FXML
+    public void openRecent(ActionEvent actionEvent) {
+        System.out.println("Clicked On Item:");
+    }
+    @FXML
+    public void about(ActionEvent actionEvent) {
+        System.out.println("Clicked On Item:");
+    }
+    @FXML
+    public void helpFile(ActionEvent actionEvent) {
+        System.out.println("Clicked On Item:");
+    }
+    @FXML
+    public void githubRepo(ActionEvent actionEvent) {
+        System.out.println("Clicked On Item:");
+    }
 	/*
 	private Main main;
 	
 	@FXML
 	private void gohomepage() throws IOException {
 	main.showMainItems();
-	
+
 	*/
 }
 
