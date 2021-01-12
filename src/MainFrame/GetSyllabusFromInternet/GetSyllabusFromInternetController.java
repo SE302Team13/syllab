ec9e7d83deaa277b4f4ede587b6c5c7df6693365
zeynepbetul <z.beetulkaya@gmail.com>
2021-01-13 01:08:35 +0300

    Added web service handler

--- src/MainFrame/GetSyllabusFromInternet/GetSyllabusFromInternetController.java
+++ src/MainFrame/GetSyllabusFromInternet/GetSyllabusFromInternetController.java
@@ -1,13 +1,18 @@
 package MainFrame.GetSyllabusFromInternet;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import com.google.gson.Gson;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import model.*;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GetSyllabusFromInternetController {
 	
@@ -39,9 +44,30 @@ public class GetSyllabusFromInternetController {
 	
 	
 	@FXML
-	public void sendRequest(ActionEvent actionEvent) {
+	public void sendRequest(ActionEvent actionEvent) throws IOException {
 		if (validate()) {
-			
+			URL url = new URL("https://syllab.azurewebsites.net/api/syllab?lang=tr&code=CE+221");
+			InputStreamReader reader = new InputStreamReader(url.openStream());
+
+			/*Gson gson = new Gson();
+
+			BufferedReader br = new BufferedReader(
+					new FileReader("E:file.json"));*/
+
+			AcademicTitle academicTitleObj = new Gson().fromJson(reader, AcademicTitle.class);
+			System.out.println(academicTitleObj.name());
+			Course courseObj = new Gson().fromJson(reader, Course.class);
+			CourseBook courseBookObj = new Gson().fromJson(reader, CourseBook.class);
+			CourseCategory courseCategoryObj = new Gson().fromJson(reader, CourseCategory.class);
+			CourseCompetency courseCompetencyObj = new Gson().fromJson(reader, CourseCompetency.class);
+			CourseType courseTypeObj = new Gson().fromJson(reader, CourseType.class);
+			EvaluationCriteria evaluationCriteriaObj = new Gson().fromJson(reader, EvaluationCriteria.class);
+			Language languageObj = new Gson().fromJson(reader, Language.class);
+			Lecturer lecturerObj = new Gson().fromJson(reader, Lecturer.class);
+			SemesterActivity semesterActivityObj = new Gson().fromJson(reader, SemesterActivity.class);
+			SemesterOptions semesterOptionsObj = new Gson().fromJson(reader, SemesterOptions.class);
+			WeeklySubject weeklySubjectObj = new Gson().fromJson(reader, WeeklySubject.class);
+
 			//Web service handler must be written here.
 		}
 	}
