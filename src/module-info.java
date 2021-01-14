81ee72a623bfdd8fbec0da0ddb945cef7cccf995
Hakan Ayaz <hakanayaz14159@outlook.com>
2021-01-14 22:49:15 +0300

    Small fix #3 GitHub logo has been replaced with a smaller one. module-info.java is adjusted to delete unintentional changes.

--- src/module-info.java
+++ src/module-info.java
@@ -10,23 +10,9 @@ module TermProject {
 	requires org.controlsfx.controls;
 	requires javafx.web;
 	requires java.desktop;
-<<<<<<< Updated upstream
-	requires gson;
 	
-=======
+	
 	opens newui;
->>>>>>> Stashed changes
-	
-	/*
-	opens MainFrame;
-	opens MainFrame.view;
-	opens MainFrame.about;
-	opens MainFrame.GetSyllabusFromInternet;
-	opens MainFrame.GitHubRepository;
-	opens MainFrame.helpFile;
-	opens MainFrame.open_;
-	opens MainFrame.OpenRecent;
-	*/
 
 	
 }
